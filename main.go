a0ff142d0fa3b1ef76d2a10c8bc11cff4c36e689
surskitt <surskitt@users.noreply.github.com>
2025-11-15 07:52:50 +0000

    feat: implement tagging torrent with tracker

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 
 	"github.com/autobrr/go-qbittorrent"
@@ -18,7 +19,69 @@ type CliArgs struct {
 }
 
 type Config struct {
-	Server string `yaml:"server" validate:"required"`
+	Server   string `yaml:"server" validate:"required"`
+	Trackers map[string][]string
+}
+
+func stringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
+}
+
+func genWebhookHandler(cfg *Config, qb *qbittorrent.Client) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		ctx := context.Background()
+
+		if err := qb.LoginCtx(ctx); err != nil {
+			log.Fatalf("could not log into client: %q", err)
+		}
+
+		infoHash := c.PostForm("infoHash")
+
+		if infoHash == "" {
+			log.Println("Error: missing infoHash")
+			c.AbortWithStatus(http.StatusBadRequest)
+		}
+
+		hashes := []string{infoHash}
+
+		torrents, err := qb.GetTorrents(qbittorrent.TorrentFilterOptions{
+			Hashes: hashes,
+		})
+
+		if err != nil {
+			log.Println("could not get torrents from client: %q", err)
+			c.AbortWithStatus(http.StatusInternalServerError)
+		}
+
+		if len(torrents) == 0 {
+			log.Println("no torrents found")
+			c.AbortWithStatus(http.StatusNotFound)
+		}
+
+		t := torrents[0]
+
+		u, err := url.Parse(t.Tracker)
+		if err != nil {
+			log.Println("could not parse tracker url: %q", err)
+			c.AbortWithStatus(http.StatusInternalServerError)
+		}
+
+		tracker := u.Hostname()
+
+		for trackerName, urls := range cfg.Trackers {
+			for _, url := range urls {
+				if url == tracker {
+					tag := "site:" + trackerName
+					qb.AddTags(hashes, tag)
+				}
+			}
+		}
+	}
 }
 
 func main() {
@@ -57,25 +120,8 @@ func main() {
 		Host: cfg.Server,
 	})
 
-	ctx := context.Background()
-
-	if err = qb.LoginCtx(ctx); err != nil {
-		log.Fatalf("could not log into client: %q", err)
-	}
-
 	r := gin.Default()
-	r.POST("/api/webhook", func(c *gin.Context) {
-		infoHash := c.PostForm("infoHash")
-
-		if infoHash == "" {
-			log.Println("Error: missing infoHash")
-			c.AbortWithStatus(http.StatusBadRequest)
-		}
-
-		hashes := []string{infoHash}
-
-		qb.AddTags(hashes, "abc")
-	})
+	r.POST("/api/webhook", genWebhookHandler(&cfg, qb))
 
 	if err = r.Run(":8080"); err != nil {
 		log.Fatal(err)
